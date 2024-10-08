2f701d0fd8f9e7e107eeee8df3c5b6e38a016837
Merve Ozel <leylakmerve@gmail.com>
2024-10-08 19:06:04 +0300

    change

---
+++ assets/js/index.js
@@ -0,0 +1,11 @@
+function calculateDiscount() {
+  let userSeconds = Number( document.getElementById('userSeconds').value);
+  let minute;
+  let second;
+  let hour;
+  let result;
+
+
+//  inputtan deger almak istiyorsan (veri.value kullanilir) dont forget baby
+
+}
